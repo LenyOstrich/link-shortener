6ff7ae69657a00b8301f790331f2d4d06b5e5729
Iuliia N Kriuchkova <leny_ostrich@mail.ru>
2024-10-07 18:26:11 +0300

    Removed extra dependenices Created test class

---
+++ src/test/java/ru/iukr/linkshortener/LinkShortenerAppTest.java
@@ -0,0 +1,13 @@
+package ru.iukr.linkshortener;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LinkShortenerAppTest {
+
+    @Test
+    void test() {
+
+    }
+}
