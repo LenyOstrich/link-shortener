789cfa3900bce239843cf1c1a52c4558fb79cbca
Iuliia N Kriuchkova <leny_ostrich@mail.ru>
2024-10-25 19:18:17 +0300

    Added custom gradle library

--- src/main/java/ru/iukr/linkshortener/LinkShortenerApp.java
+++ src/main/java/ru/iukr/linkshortener/LinkShortenerApp.java
@@ -2,10 +2,12 @@ package ru.iukr.linkshortener;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import ru.iukr.loggingstarter.LoggingStarterAutoConfiguration;
 
 @SpringBootApplication
 public class LinkShortenerApp {
     public static void main(String[] args) {
+        LoggingStarterAutoConfiguration.println("Test");
         SpringApplication.run(LinkShortenerApp.class, args);
     }
 }
